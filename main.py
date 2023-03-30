d0c6dcd8db664a2f08241b9f699a9d0cdd79e4e9
Max <riabytsa@gmail.com>
2023-03-30 10:55:42 +0300

    Added sorting

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ def read_data():
     return data
 
 
+def sort_by_area(data):
+    return sorted(data, key=lambda x: float(x[1]))
+
+
+def sort_by_population(data):
+    return sorted(data, key=lambda x: float(x[2]))
+
+
 def main():
     data = read_data()
+    print(f'Sorted by area: {sort_by_area(data)}')
+    print(f'Sorted by population: {sort_by_population(data)}')
 
 
 if __name__ == '__main__':
