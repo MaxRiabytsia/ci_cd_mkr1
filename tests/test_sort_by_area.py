0f6e609d56298a690d567feff64b0c7f29c11e59
Max <riabytsa@gmail.com>
2023-03-30 11:38:09 +0300

    Fixed file paths

--- tests/test_sort_by_area.py
+++ tests/test_sort_by_area.py
@@ -1,12 +1,13 @@
 import pytest
+import os
 
 from main import sort_by_area, read_data
 
 
 @pytest.mark.parametrize('file_name, expected', [
-    ["../data.txt", [['poland', '400000', '42.32'], ['ukraine', '603700', '43.79'], ['usa', '20000000', '311.56']]],
-    ["../data2.txt", [['spain', '400000', '66.67'], ['germany', '440000', '68.37'], ['france', '590000', '70.21']]],
+    ["data.txt", [['poland', '400000', '42.32'], ['ukraine', '603700', '43.79'], ['usa', '20000000', '311.56']]],
+    ["data2.txt", [['spain', '400000', '66.67'], ['germany', '440000', '68.37'], ['france', '590000', '70.21']]],
 ])
 def test_sort_by_area(file_name, expected):
-    data = read_data(file_name)
+    data = read_data(os.path.join("..", file_name))
     assert sort_by_area(data) == expected
--- tests/test_sort_by_population.py
+++ tests/test_sort_by_population.py
@@ -1,12 +1,13 @@
 import pytest
+import os
 
 from main import sort_by_population, read_data
 
 
-@pytest.mark.parametrize('data_file, expected', [
-    ['../data.txt', [['poland', '400000', '42.32'], ['ukraine', '603700', '43.79'], ['usa', '20000000', '311.56']]],
-    ['../data2.txt', [['spain', '400000', '66.67'], ['germany', '440000', '68.37'], ['france', '590000', '70.21']]],
+@pytest.mark.parametrize('file_name, expected', [
+    ['data.txt', [['poland', '400000', '42.32'], ['ukraine', '603700', '43.79'], ['usa', '20000000', '311.56']]],
+    ['data2.txt', [['spain', '400000', '66.67'], ['germany', '440000', '68.37'], ['france', '590000', '70.21']]],
 ])
-def test_sort_by_area(data_file, expected):
-    data = read_data(data_file)
+def test_sort_by_area(file_name, expected):
+    data = read_data(os.path.join("..", file_name))
     assert sort_by_population(data) == expected
